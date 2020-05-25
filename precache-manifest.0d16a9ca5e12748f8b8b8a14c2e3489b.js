439449de21df4d43ef52e8fe1422042401841e9e
Realoj <stephenojwang1040@gmail.com>
2020-05-25 03:54:30 +0300

    Updates

---
+++ precache-manifest.0d16a9ca5e12748f8b8b8a14c2e3489b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f30372356a0d69081ce9807d037bb42c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1b599ef33f0fee0051e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fa8c21ebe0b150e72ccd",
+    "url": "/static/js/2.1b7b7e22.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.1b7b7e22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1b599ef33f0fee0051e",
+    "url": "/static/js/main.9983ef25.chunk.js"
+  },
+  {
+    "revision": "d3dc11b3cebd24cf2173",
+    "url": "/static/js/runtime-main.994a4bb0.js"
+  }
+]);
