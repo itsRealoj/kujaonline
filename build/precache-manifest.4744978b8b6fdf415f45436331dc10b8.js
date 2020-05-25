325f912a45b54bd96e8df937c6d70ccbe923f7b1
Realoj <stephenojwang1040@gmail.com>
2020-05-25 04:53:19 +0300

    modified entry point

---
+++ build/precache-manifest.4744978b8b6fdf415f45436331dc10b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3732b1f63f8fce03dbbb2f5475bc0ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb323e2bc7a8c6d8ca37",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fa8c21ebe0b150e72ccd",
+    "url": "/static/js/2.1b7b7e22.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.1b7b7e22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb323e2bc7a8c6d8ca37",
+    "url": "/static/js/main.abf93923.chunk.js"
+  },
+  {
+    "revision": "d3dc11b3cebd24cf2173",
+    "url": "/static/js/runtime-main.994a4bb0.js"
+  }
+]);
